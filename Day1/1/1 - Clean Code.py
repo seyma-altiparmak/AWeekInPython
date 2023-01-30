6e28d9704831bf3f2c84935b144d943ddc116a68
Seyma Altiparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2023-01-30 22:37:31 +0300

    Day1 Clean Code Txt

---
+++ Day1/1/1 - Clean Code.py
@@ -0,0 +1,21 @@
+     #What is #CleanCode?
+
+#readability = Simple-to-understand
+#changeability = Simple-Changeable
+#extensibility = Simple-Extensible
+#maintainability = Basic-Maintenance(Repair)
+
+#Code that stays away from complex structure.
+#polymorphism
+#Configure and Dynamic code writing.
+#correct naming
+#Descriptive comment lines
+#Continuous-Tested : Test results
+     # -quick-standalone-retestable
+
+#Code Smells =
+     # Rigidity (Hard-Modified) -> effect
+     # Fragility (Hard-Modified) -> corruption
+     # Immobility
+     # Needless Complexity , Needless Repetition
+     # Opacity (Hard-to-Read)
