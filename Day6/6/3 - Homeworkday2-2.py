88eb7752c2dbe690c2fd3bd05716f58ba50a3b38
Seyma Altiparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2023-02-04 18:56:55 +0300

    HW-D2

---
+++ Day6/6/3 - Homeworkday2-2.py
@@ -0,0 +1,12 @@
+#Question 2
+#Logical Op.
+import random
+
+a = random.randint(1,10)
+b = random.randint(1,20)
+c = random.randint(1,30)
+d = random.randint(1,40)
+
+a != b and c != d
+a != b or c != d
+not a!=b #Discrete math it named negation
