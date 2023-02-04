89b2c616b710ec9d738f2f1a48e8d62541c7d63a
Seyma Altiparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2023-02-04 18:52:48 +0300

    Q1-D2

---
+++ Day6/6/2 - Homeworkday2.py
@@ -0,0 +1,22 @@
+#DAY2
+#Question-1
+import random
+
+deneme = int("question".__len__())
+number = random.randint(1,10)
+
+print("Lenght is : " + str(deneme))
+print("Number is : " + str(number))
+
+#equal
+print(deneme == number)
+#not equal
+print(deneme != number)
+#smaller
+print(deneme < number)
+#bigger
+print(deneme > number)
+#equal and smaller
+print(deneme <= number)
+#equal and bigger
+print(deneme >= number)
