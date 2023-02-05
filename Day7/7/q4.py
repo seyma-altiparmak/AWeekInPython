d80d85f6c46e423d1b4578fab4dc63bc9f69f66f
Seyma Altiparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2023-02-05 16:36:37 +0300

    q4 updated

--- Day7/7/q4.py
+++ Day7/7/q4.py
@@ -1,10 +1,11 @@
 #Question-4
 Integer = int(input("Give me integer:"))
 
-for i in Integer:
-    try:
-        print(i)
-    except TypeError:
-        print("not iterable")
-    except:
-        print("You CAN'T CATCH the point")
+
+try:
+    for i in Integer:
+     print(i)
+except TypeError:
+    print("not iterable")
+except:
+    print("You CAN'T CATCH the point")
